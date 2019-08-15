9c909195672c7d88bf5bb3edc1def38b4864df76
yumetodo <yume-wikijp@live.jp>
2019-08-16 01:01:14 +0900

    feat: now you can get uid

--- src/imageInfo.ts
+++ src/imageInfo.ts
@@ -1,5 +1,22 @@
 export interface ImageInfo {
-  /** url of image */
+  /**
+   * When you have shared album URL that can get via Google Photos API, that shold be like below:
+   *
+   * `https://photos.app.goo.gl/${shortAlbumUID}`
+   *
+   * When you access to the URL, it will be redirected. Then, you can get `albumUID` and `key`
+   *
+   * ```js
+   * console.log(await fetch(`https://photos.app.goo.gl/${shortAlbumUID}`).then(r => r.url)); // => https://photos.google.com/share/${albumUID}?key=${key}
+   * ```
+   * Also, you can get the `uid` that identify the photo you select. The `uid` can get via `GooglePhotos.Album.fetchImageUrls`
+   *
+   * Now, you can get the URL of the photo page.
+   *
+   * `https://photos.google.com/share/${albumUID}/photo/${uid}?key=${key}`
+   */
+  uid: string;
+  /** url of image binary */
   url: string;
   width: number;
   height: number;
