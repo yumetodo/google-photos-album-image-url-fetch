bad851f6d878d1390f1345d51ed5258c8a05a507
yumetodo <yume-wikijp@live.jp>
2019-08-17 23:50:56 +0900

    feat: add extractAppended to compare

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { AbortSignal } from 'abort-controller';
+import equal from 'fast-deep-equal';
 import { getSharedAlbumHtml, parsePhase1, parsePhase2, parsePhase3 } from './impl';
 import { ImageInfo as info } from './imageInfo';
 export namespace GooglePhotos {
@@ -15,6 +16,9 @@ export namespace GooglePhotos {
         return null;
       }
       return parsePhase3(ph2);
+    }
+    export function extractAppended(before: ImageInfo[], after: ImageInfo[]) {
+      return after.filter(a => typeof before.find(v => equal(v, a)) === 'undefined');
     }
   }
 }
--- test/main.test.ts
+++ test/main.test.ts
@@ -3,9 +3,23 @@ import { GooglePhotos } from '../src/index';
 import expected from './expected.json';
 import { GooglePhotosSharedAlbumURL } from './constant'
 describe('test', () => {
-  it('test', async () => {
+  it('fetchImageUrls', async () => {
     const re = await GooglePhotos.Album.fetchImageUrls(GooglePhotosSharedAlbumURL);
     expect(re).not.toBeNull();
     expect(re).toEqual(expected);
+  });
+  it('extractAppended', () => {
+    const after: GooglePhotos.Album.ImageInfo[] = expected;
+    const before: GooglePhotos.Album.ImageInfo[] = [];
+    const appended: GooglePhotos.Album.ImageInfo[] = [];
+    for (const e of after) {
+      if (Math.random() < 0.5) {
+        before.push(e);
+      } else {
+        appended.push(e);
+      }
+    }
+    const re = GooglePhotos.Album.extractAppended(before, after);
+    expect(re).toEqual(appended);
   });
 });
