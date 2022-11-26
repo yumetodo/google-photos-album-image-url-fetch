a7f72aad4d1fed30a078e2f0df9df57a3c309445
yumetodo <yume-wikijp@live.jp>
2022-11-26 19:47:54 +0900

    chore(test): follow expected.json structure change(#19)

--- test/main.test.ts
+++ test/main.test.ts
@@ -3,7 +3,7 @@ import * as GooglePhotosAlbum from '../src/index';
 // @ts-ignore: Cannot find module '../src/expected.json'. Consider using '--resolveJsonModule' to import module with '.json' extension
 import expected from '../src/expected.json';
 import { googlePhotosSharedAlbumURL } from '../src/constant';
-import { splitResult } from '../src/split_result';
+import { splitResult, ImageInfoLike } from '../src/split_result';
 describe('test', () => {
   it('fetchImageUrls', async () => {
     const re = await GooglePhotosAlbum.fetchImageUrls(googlePhotosSharedAlbumURL);
@@ -19,7 +19,11 @@ describe('test', () => {
     }
   });
   it('extractAppended', () => {
-    const after: GooglePhotosAlbum.ImageInfo[] = expected;
+    const after: GooglePhotosAlbum.ImageInfo[] = expected.map((e: ImageInfoLike) => {
+      const ret = { ...e };
+      ret.url = e.url[0];
+      return ret as GooglePhotosAlbum.ImageInfo;
+    });
     const before: GooglePhotosAlbum.ImageInfo[] = [];
     const appended: GooglePhotosAlbum.ImageInfo[] = [];
     for (const e of after) {
